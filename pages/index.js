a0d5423c2ce49dc26780faeab1f7cb6bcf9e2c2c
yayinchang <irene84111@gmail.com>
2023-08-13 23:28:30 +0800

    fix: revised index

--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,71 @@
 import React from 'react';
 import { getCustomPages, queries } from '@/data';
+import { useInView } from 'react-intersection-observer';
 import cx from 'classnames';
 import BrandTitle from '@/components/BrandTitle';
-import ScrollDown from '@/components/ScrollDown';
+import ScrollArrows from '@/components/ScrollArrows';
 import CustomPortableText from '@/components/CustomPortableText';
-import CustomLink from '@/components/CustomLink';
 import theme from '@/styles/theme';
 
-function IndexPage({ data }) {
+const Hero = ({ data = {} }) => {
 	const { page, site } = data;
 	const { intro, aboutTogglerLabel } = page;
+	const { ref, inView } = useInView({
+		triggerOnce: true,
+		threshold: 0,
+	});
 
 	return (
 		<>
-			<div className="homepage">
-				<section className="homepage-hero f-v f-j-b gap-gutter">
-					<h1 className="screen-reader-only">{site.title}</h1>
-					<BrandTitle />
-					{(intro || aboutTogglerLabel) && (
-						<div className="homepage-hero-intro f-v f-a-s gap-8">
-							{intro && (
-								<CustomPortableText blocks={intro} classNames="cr-white" />
-							)}
-							{aboutTogglerLabel && (
-								<button
-									className="btn btn--text js-about-toggler"
-									aria-label={`Learn more about ${site.title}`}
-								>
-									{aboutTogglerLabel}
-								</button>
-							)}
-						</div>
-					)}
-					<ScrollDown />
-				</section>
-			</div>
+			<section className="homepage-hero f-v f-j-b gap-gutter">
+				<h1 className="homepage-hero-title">
+					<BrandTitle>{site.title}</BrandTitle>
+				</h1>
+				{(intro || aboutTogglerLabel) && (
+					<div
+						ref={ref}
+						className={cx('homepage-hero-intro f-v f-a-s gap-8', {
+							'is-animated': inView,
+						})}
+						data-animate="up"
+					>
+						{intro && <CustomPortableText blocks={intro} />}
+						{aboutTogglerLabel && (
+							<button
+								className="btn btn--text js-about-toggler"
+								aria-label={`Learn more about ${site.title}`}
+							>
+								{aboutTogglerLabel}
+							</button>
+						)}
+					</div>
+				)}
+				<ScrollArrows />
+			</section>
 			<style jsx>{`
-				.homepage {
+				.homepage-hero {
 					position: relative;
+					padding: var(--s-gutter-lg);
+					min-height: var(--s-vp-height);
 
-					//min-height: calc((var(--s-vp-height) - var(--s-footer)) * 2);
-
-					&-hero {
-						padding: var(--s-gutter-lg);
-						min-height: var(--s-vp-height);
-
-						&-title {
-							font-size: 13vw;
-							font-weight: 600;
-							line-height: 1;
-							letter-spacing: -0.1rem;
-							text-transform: uppercase;
-						}
+					&-title {
+						font-size: 16vw;
+						font-weight: 300;
+						line-height: 1;
+						letter-spacing: -0.1rem;
+						white-space: nowrap;
 					}
 				}
 			`}</style>
 		</>
+	);
+};
+
+function IndexPage({ data }) {
+	return (
+		<div className="homepage">
+			<Hero data={data} />
+		</div>
 	);
 }
 
